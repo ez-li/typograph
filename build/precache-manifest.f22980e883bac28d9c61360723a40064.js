2ddbc3bedb68f28903fb470ea98ee3aba76249e9
ez-li <eileenli.sf@gmail.com>
2020-02-18 23:42:00 -0800

    Cleaning up template files

---
+++ build/precache-manifest.f22980e883bac28d9c61360723a40064.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a6907dfe5429706ff747a135d51813b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e89275876c213b7c98dc",
+    "url": "/static/css/main.7bbca5cd.chunk.css"
+  },
+  {
+    "revision": "c586403b1f342a4a93ed",
+    "url": "/static/js/2.16dedccb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.16dedccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e89275876c213b7c98dc",
+    "url": "/static/js/main.4dc90cc8.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
