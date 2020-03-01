1207d26aca66cc814d6ff99b35a6f384af609c76
ez-li <eileenli.sf@gmail.com>
2020-03-01 00:23:34 -0800

    Stylizer changes input text style, italic, bold

--- src/FontItem.js
+++ src/FontItem.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FontItem.css';
 
 function FontItem(props) {
 
 	const { font } = props;
+
+	const [ { currentStyle, currentWeight }, setState] = useState({
+			currentStyle: "normal",
+			currentWeight:	""
+	});
+
+	const updateStyle = (e) => {
+		if (e.target.value === "bold") {
+			setState({
+				currentStyle: '',
+				currentWeight: e.target.value
+			});
+		} else {
+			setState({
+				currentStyle: e.target.value,
+				currentWeight: ''
+			});
+		}
+	}
 
   return (
     <div className="font-item">    
@@ -11,14 +30,18 @@ function FontItem(props) {
 				type="text"
 				name={font}
 				placeholder={font}
-				style={{fontFamily: font}}
+				style={{fontFamily: font, fontStyle: currentStyle, fontWeight: currentWeight}}
+				maxLength="30"
+
 			/>
 			<div>
 				<span>See it in</span> 
-				<select id="font-styler">
-					<option value="regular">Regular</option>
+				<select 
+					id="font-styler"
+					onChange={(e) => updateStyle(e)}>
+					<option value="normal">Regular</option>
 					<option value="italic">Italic</option>
-					<option value="italic">Bold</option>
+					<option value="bold">Bold</option>
 				</select>
 			</div>
 		</div>
